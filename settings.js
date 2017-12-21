6c7fd0df3998f1c9a1788af4c799d324df97dd1d
andr435 <andr435@gmail.com>
2017-12-22 01:37:22 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,39 +5,39 @@ var settings = {
             'body': {
                 'simple': [WORK, CARRY, MOVE],
                 'advanced': [WORK, WORK, CARRY, CARRY, MOVE, MOVE],
-                'expert': [WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE],
-                'type_by_energy': {
-                    'advanced': 700,
-                    'expert': 2000
-                },
-                'role': 'harvester'
+                'expert': [WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE]
             },
-            'upgrader': {
-                'amount_on_map': 3,
-                'body': {
-                    'simple': [WORK, CARRY, MOVE],
-                    'advanced': [WORK, WORK, CARRY, CARRY, MOVE, MOVE],
-                    'expert': [WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE],
-                    'type_by_energy': {
-                        'advanced': 700,
-                        'expert': 2000
-                    },
-                    'role': 'upgrader'
-                }
+            'type_by_energy': {
+                'advanced': 700,
+                'expert': 2000
             },
-            'builder': {
-                'amount_on_map': 3,
-                'body': {
-                    'simple': [WORK, CARRY, MOVE],
-                    'advanced': [WORK, WORK, CARRY, CARRY, MOVE, MOVE],
-                    'expert': [WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE],
-                    'type_by_energy': {
-                        'advanced': 700,
-                        'expert': 2000
-                    },
-                    'role': 'upgrader'
-                }
-            }
+            'role': 'harvester'
+        },
+        'upgrader': {
+            'amount_on_map': 3,
+            'body': {
+                'simple': [WORK, CARRY, MOVE],
+                'advanced': [WORK, WORK, CARRY, CARRY, MOVE, MOVE],
+                'expert': [WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE]
+            },
+            'type_by_energy': {
+                'advanced': 700,
+                'expert': 2000
+            },
+            'role': 'upgrader'
+        },
+        'builder': {
+            'amount_on_map': 3,
+            'body': {
+                'simple': [WORK, CARRY, MOVE],
+                'advanced': [WORK, WORK, CARRY, CARRY, MOVE, MOVE],
+                'expert': [WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE]
+            },
+            'type_by_energy': {
+                'advanced': 700,
+                'expert': 2000
+            },
+            'role': 'upgrader'
         }
     }
 };
